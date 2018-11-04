dbd9f28f762700fae3c7b79e762ef67aa3b49c74
Singh <Anmol.Singh@nike.com>
2018-11-04 08:54:48 -0800

    adding new class

--- src/main/java/com/main/DriverClasee.java
+++ src/main/java/com/main/DriverClasee.java
@@ -1,10 +1,26 @@
 package com.main;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DriverClasee {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		openBrowser();
 
 	}
 
+	
+	public static void openBrowser() {
+		
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.expedia.com/");
+		
+		driver.quit();
+	}
 }
